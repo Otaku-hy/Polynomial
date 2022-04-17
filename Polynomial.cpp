dd89b0b833b3c14fd756465e39fc1ecca01ad018
zhanghangyu <zhanghangyu86@gmail.com>
2022-04-17 15:46:44 +0800

    add

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -4,6 +4,19 @@ Polynomial::Polynomial(/* args */)
 {
 }
 
+Polynomial::Polynomial(const Polynomial &other) : poly_size_(other.poly_size_)
+{
+    head_node_ = std::make_shared<PolynomialNode>();
+    PolynomialNode::SharedPtr this_traverse_ptr = head_node_;
+    PolynomialNode::SharedPtr other_traverse_ptr = other.head_node_->next;
+    while (other_traverse_ptr)
+    {
+        this_traverse_ptr->next = std::make_shared<PolynomialNode>(other_traverse_ptr->expo, other_traverse_ptr->coef);
+        this_traverse_ptr = this_traverse_ptr->next;
+        other_traverse_ptr = other_traverse_ptr->next;
+    }
+}
+
 Polynomial::~Polynomial()
 {
 }
@@ -58,4 +71,29 @@ void Polynomial::InsertNode(const PolynomialNode::SharedPtr &node)
             traverse_pre_ptr->next = node;
         }
     }
+}
+
+Polynomial &Polynomial::operator=(const Polynomial &other)
+{
+    head_node_->next = nullptr;
+    poly_size_ = other.poly_size_;
+
+    PolynomialNode::SharedPtr this_traverse_ptr = head_node_;
+    PolynomialNode::SharedPtr other_traverse_ptr = other.head_node_->next;
+    while (other_traverse_ptr)
+    {
+        this_traverse_ptr->next = std::make_shared<PolynomialNode>(other_traverse_ptr->expo, other_traverse_ptr->coef);
+        this_traverse_ptr = this_traverse_ptr->next;
+        other_traverse_ptr = other_traverse_ptr->next;
+    }
+
+    return *this;
+}
+
+Polynomial Polynomial::operator+(const Polynomial &other)
+{
+    PolynomialNode::SharedPtr others_travese_ptr = other.head_node_->next;
+    Polynomial result(*this);
+
+    
 }
--- Polynomial.hpp
+++ Polynomial.hpp
@@ -5,24 +5,30 @@ struct PolynomialNode
 {
     using SharedPtr = std::shared_ptr<PolynomialNode>;
 
-    double expo = 0;
-    uint32_t coef = 0;
+    int32_t expo = 0;
+    double coef = 0;
     SharedPtr next = nullptr;
+
+    PolynomialNode(int32_t _expo, double _coef) : expo(_expo), coef(_coef), next(nullptr){};
 };
 
 class Polynomial
 {
 private:
     size_t poly_size_ = 0;
-    PolynomialNode::SharedPtr head_node_ = nullptr;
+    PolynomialNode::SharedPtr head_node_ = std::make_shared<PolynomialNode>();
 
 public:
     using SharedPtr = std::shared_ptr<Polynomial>;
 
     Polynomial(/* args */);
+    Polynomial(const Polynomial &other);
     ~Polynomial();
 
     bool IsEmpty();
     void PrintPolynomial();
-    void InsertNode(const PolynomialNode::SharedPtr& node);
+    void InsertNode(const PolynomialNode::SharedPtr &node);
+
+    Polynomial &operator=(const Polynomial &other);
+    Polynomial operator+(const Polynomial &other);
 };
